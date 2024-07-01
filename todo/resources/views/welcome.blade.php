f87a7f2bf264e6bf52fcd6698c62af5da0054fc7
martin-lian <martin.agassi@gmail.com>
2024-07-01 15:54:29 +0530

    edit

--- todo/resources/views/welcome.blade.php
+++ todo/resources/views/welcome.blade.php
@@ -6,6 +6,25 @@
     <title>Laravel Livewire</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     @livewireStyles()
+    <style>
+    h1{
+        text-align: center;
+    }
+   *{
+        padding:auto;
+        margin:auto;
+        text-align:centre;
+    }
+    .container{
+        margin: auto;
+        width: 70%;
+        border: 3px solid green;
+        padding: 50px;
+    }
+    body{
+        margin:50px;
+    }
+    </style>
 </head>
 
 <body>
