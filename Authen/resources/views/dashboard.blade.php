555c67b7eba7454804b8385372f94d6c71c5ec55
martin-lian <martin.agassi@gmail.com>
2024-07-03 17:51:29 +0530

    New

---
+++ Authen/resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+
+    <form>
+        <div class="form-group">
+            <label for="exampleFormControlInput1"></label>
+            <input wire:model='title' type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Title" >
+            @error('title')
+                <span class="text-danger">{{$message}}</span>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlInput2"></label>
+            <textarea wire:model='body' type="email" class="form-control" id="exampleFormControlInput2"  placeholder="Enter Task"></textarea>
+            @error('body')
+            <span class="text-danger">{{$message}}</span>
+            @enderror
+        </div>
+        <button wire:click.prevent="store()" class="btn btn-success mt-3">Save</button>
+    </form>
+    
+</x-app-layout>
+
